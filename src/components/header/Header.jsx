adb83316e3287ed6c90577a423e5deff9866476b
CJRS2 <reyes.christian@pucp.pe>
2022-09-20 02:53:27 -0500

    Arreglando el Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,23 +3,30 @@ import './header.css'
 import {BsFacebook} from 'react-icons/bs'
 import {BsInstagram} from 'react-icons/bs'
 import {AiFillTwitterCircle} from 'react-icons/ai'
+import {BsCartPlusFill} from 'react-icons/bs'
 
 
 const Header = () => {
   return (
     <div className='header_container' id='Header'>
       <nav className='nav_container'>
-      <a href="https://www.facebook.com" className='nav_menu' ><BsFacebook style={{ fontSize: '30px' }} />  </a>
-      <a href="https://twitter.com/?lang=en" className='nav_menu'><BsInstagram style={{ fontSize: '30px' }} /> </a>
-      <a href="https://www.instagram.com" className='nav_menu'><AiFillTwitterCircle style={{ fontSize: '30px' }} />   </a>
+      <a href="https://www.facebook.com" target="_blank" className='nav_menu' ><BsFacebook style={{ fontSize: '30px' }} /></a>
+      <a href="https://www.instagram.com" target="_blank" className='nav_menu'><BsInstagram style={{ fontSize: '30px' }} /></a>
+      <a href="https://twitter.com/?lang=en" target="_blank" className='nav_menu'><AiFillTwitterCircle style={{ fontSize: '30px' }} /></a>
       </nav>
       
       <nav className='nav_container'>
       <a href="#contact" className='nav_menu' >Conócenos</a>
       <a href="#work" className='nav_menu' >Trabaja con nosotros</a>
+      
+      </nav>
+
+      <nav className='nav_container'>
       <button className="btn">Registrarse</button>
       <button className="btn">Iniciar sesión</button>
+      <a href="https://www.facebook.com" target="_blank" className='nav_menu' ><BsCartPlusFill style={{ fontSize: '30px' }} /></a>
       </nav>
+      
     </div>
   )
 }
