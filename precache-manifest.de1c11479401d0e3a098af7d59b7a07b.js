2c7e8169c50105c7072360fbc0c6b77783e36b97
CJRS1 <reyes.christian@pucp.pe>
2022-10-12 00:57:21 -0700

    Updates

---
+++ precache-manifest.de1c11479401d0e3a098af7d59b7a07b.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccca403bb3b200cfa8ba",
+    "url": "/Cinemeros/static/css/main.c078dfc9.chunk.css"
+  },
+  {
+    "revision": "ccca403bb3b200cfa8ba",
+    "url": "/Cinemeros/static/js/main.50bcd9ba.chunk.js"
+  },
+  {
+    "revision": "21f65efb6ebd7d9dbcd3",
+    "url": "/Cinemeros/static/js/runtime~main.7d4ad130.js"
+  },
+  {
+    "revision": "53cfac9a0e4945b728b9",
+    "url": "/Cinemeros/static/js/2.67767e37.chunk.js"
+  },
+  {
+    "revision": "b09c503b1e6ac020d9fa868a8d323e81",
+    "url": "/Cinemeros/static/media/Cinemeros.489aa531.jpg"
+  },
+  {
+    "revision": "1a776c6372aa57ac183d1d0c71460188",
+    "url": "/Cinemeros/static/media/Promocion1.6cc6725a.jpg"
+  },
+  {
+    "revision": "0aeed15176f62db925efef828edc1916",
+    "url": "/Cinemeros/static/media/Promocion2.142f7439.jpg"
+  },
+  {
+    "revision": "3ba16451bf612a3b311e2fa50ffad5a0",
+    "url": "/Cinemeros/static/media/Comunicado.66d1dec8.jpg"
+  },
+  {
+    "revision": "66584706702a18261ca56052220b6f6e",
+    "url": "/Cinemeros/static/media/agua.5d06d275.png"
+  },
+  {
+    "revision": "aa202f367189745548cf579044cef0c7",
+    "url": "/Cinemeros/static/media/comboduo.e3725c4f.png"
+  },
+  {
+    "revision": "1e654cdf1e10420fb9ed7a84dc830023",
+    "url": "/Cinemeros/static/media/combopersonal1.0a8cdd8f.png"
+  },
+  {
+    "revision": "5e4bc23e26dd89122cb34402e2147827",
+    "url": "/Cinemeros/static/media/combopersonal2.392d4688.png"
+  },
+  {
+    "revision": "b2ca845ee4a84d6987f21874d18a25f5",
+    "url": "/Cinemeros/static/media/combotrio.f2ee57d9.png"
+  },
+  {
+    "revision": "5e42850ec3b0781720d2adc68b135ebe",
+    "url": "/Cinemeros/static/media/gaseosagrande.f8ddfa65.png"
+  },
+  {
+    "revision": "15d1236eb3089d7d6720179a935fd2cb",
+    "url": "/Cinemeros/static/media/gaseosamediana.e29900bd.png"
+  },
+  {
+    "revision": "70de6025e41e19ee546bec9d233c2ab3",
+    "url": "/Cinemeros/static/media/papas.95bb31f8.png"
+  },
+  {
+    "revision": "8cf60ab7de94286c54ec5d4522a0b52b",
+    "url": "/Cinemeros/static/media/Promo1.470db84f.png"
+  },
+  {
+    "revision": "87cc8251bb9035d47ae4b2b2eaed468b",
+    "url": "/Cinemeros/static/media/Promo2.9e4e2958.png"
+  },
+  {
+    "revision": "9054c9a8944405701e3f0a7601808a00",
+    "url": "/Cinemeros/static/media/Promo3.ab66aa5b.png"
+  },
+  {
+    "revision": "850739fc8fb5cab464179323b01a9347",
+    "url": "/Cinemeros/static/media/Promo4.9bcf4c4b.png"
+  },
+  {
+    "revision": "5396eb519bca08e8f968149a8233245f",
+    "url": "/Cinemeros/static/media/Promo5.a8d6cfa8.png"
+  },
+  {
+    "revision": "fa8318dd73a35b0addd93905c499c61a",
+    "url": "/Cinemeros/static/media/Promo6.6b35698f.png"
+  },
+  {
+    "revision": "78ee2f61caff5b28302751bbe196d5f2",
+    "url": "/Cinemeros/index.html"
+  }
+];
